40e4bfc07d0b46f1254a80708f739583146b074c
Mateusz Kowalczyk <mateusz.kowalczyk24@gmail.com>
2020-03-11 21:06:16 +0100

    Init project

---
+++ app/src/main/java/dev/mateuszkowalczyk/treebuilder/TreebuilderApplication.java
@@ -0,0 +1,13 @@
+package dev.mateuszkowalczyk.treebuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreebuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreebuilderApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/dev/mateuszkowalczyk/treebuilder/TreebuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mateuszkowalczyk.treebuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreebuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
