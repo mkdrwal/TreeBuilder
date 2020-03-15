a3fe64f0e8f7901d1c20d690ad407eb613209aff
Mateusz Kowalczyk <mateusz.kowalczyk24@gmail.com>
2020-03-15 22:41:23 +0100

    add expand and counting leaf

--- frontend/src/app/tree/components/tree-node/tree-node.component.ts
+++ frontend/src/app/tree/components/tree-node/tree-node.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, OnDestroy, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {TreeNode} from "../../interface/tree-node";
 import {EnvService} from "../../../core/service/env.service";
 import {HttpClient} from "@angular/common/http";
@@ -12,11 +12,17 @@ import {Subscription} from "rxjs";
     styleUrls: ['./tree-node.component.scss']
 })
 export class TreeNodeComponent implements OnInit, OnDestroy, AfterViewInit {
+    @ViewChildren(TreeNodeComponent) components: QueryList<TreeNodeComponent>;
     @Input() node: TreeNode;
-    @Input() parent: TreeNode;
+    expanded = true;
 
     valueInput = new FormControl();
     private subscription: Subscription;
+    private leafValue: number;
+
+    @Input() update() {
+        this.updateNode();
+    }
 
     constructor(
         private envService: EnvService,
@@ -25,27 +31,58 @@ export class TreeNodeComponent implements OnInit, OnDestroy, AfterViewInit {
     }
 
     ngOnInit(): void {
+        this.prepareChildren();
+        this.updateNode()
+    }
+
+    updateNode() {
         if (!(this.node.id)) {
             this.getId();
+        }
+
+        if (this.components) {
+            this.components.forEach(value => {
+                value.update();
+            });
+        }
+
+        if (this.node.children.length == 0 && !!this.node.parentNode) {
+            this.countLeafValue();
+        } else {
+            this.leafValue = null;
+        }
     }
-  }
 
   createChild() {
-    this.node.children.push(
-        {
-            parent: this.node.id,
-            children: new Array<TreeNode>(),
-            value: null
-        }
-    );
+      this.expanded = true;
+      this.node.children.push(
+          {
+              parent: this.node.id,
+              parentNode: this.node,
+              children: new Array<TreeNode>(),
+              value: null
+          }
+      );
+      this.updateNode();
   }
 
-    private getId() {
-        this.httpClient.post(this.envService.getApiUrl("/node"), {"parentId": this.parent.id}).subscribe(
+    delete() {
+        this.httpClient.delete(this.envService.getApiUrl(`/node/${this.node.id}`)).subscribe(
             value => {
-                this.node.id = <number>value;
+                this.node.parentNode.children.forEach(
+                    (node, index) => {
+                        if (this.node === node) {
+                            this.node.parentNode.children.splice(index, 1);
+                        }
+                    }
+                );
+                console.log("deleted");
             }
         );
+    }
+
+    expand() {
+        this.expanded = !this.expanded;
     }
 
     ngAfterViewInit(): void {
@@ -60,17 +97,10 @@ export class TreeNodeComponent implements OnInit, OnDestroy, AfterViewInit {
             );
     }
 
-    delete() {
-        this.httpClient.delete(this.envService.getApiUrl(`/node/${this.node.id}`)).subscribe(
+    private prepareChildren() {
+        this.node.children.forEach(
             value => {
-                this.parent.children.forEach(
-                    (node, index) => {
-                        if (this.node === node) {
-                            this.parent.children.splice(index, 1);
-                        }
-                    }
-                );
-                console.log("deleted");
+                value.parentNode = this.node
             }
         );
     }
@@ -79,11 +109,29 @@ export class TreeNodeComponent implements OnInit, OnDestroy, AfterViewInit {
         this.subscription.unsubscribe();
     }
 
+    private getId() {
+        this.httpClient.post(this.envService.getApiUrl("/node"), {"parentId": this.node.parent}).subscribe(
+            value => {
+                this.node.id = <number>value;
+            }
+        );
+    }
+
     private updateValue(value: any) {
         this.httpClient.put(this.envService.getApiUrl(`/node/${this.node.id}`), {"value": value}).subscribe(
             value => {
+                this.updateNode();
                 console.log("updated");
             }
         );
+    }
+
+    private countLeafValue() {
+        console.log("counting leaf value");
+        this.leafValue = this.countParent(this.node.parentNode);
+    }
+
+    private countParent(node: TreeNode) {
+        return node.value + (!!node.parentNode ? this.countParent(node.parentNode) : 0);
     }
 }
--- frontend/src/app/tree/interface/tree-node.ts
+++ frontend/src/app/tree/interface/tree-node.ts
@@ -1,6 +1,7 @@
 export interface TreeNode {
     id?: number;
     parent: number;
+    parentNode?: TreeNode;
     value: number;
     children: Array<TreeNode>;
 }
